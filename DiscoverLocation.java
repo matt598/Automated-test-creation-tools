cf39e86afa76592b6ddb36119e97c2a8f4a991c4
Matt Sauer <matt598@users.noreply.github.com>
2015-10-12 12:27:50 -0500

    Update DiscoverLocation.java

--- DiscoverLocation.java
+++ DiscoverLocation.java
@@ -4,8 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
-
 import javax.swing.*;
+
+//will make a new version that updates when the mouse is clicked.
 public class DiscoverLocation
 {
     public static void main(String[] args) throws InterruptedException{
